a281d25ae19eef2825f55a5279654c2e8d771cb4
Piyush Bajaj <45912468+piyush-bajaj@users.noreply.github.com>
2020-04-25 19:48:07 +0530

    functional to class component

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./main-page.css";
 import Header from "./header";
 
-function App() {
-  return (
-    <div className="container">
-      <Header subtitle="Providing houses world wide"></Header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <Header subtitle="Providing houses world wide"></Header>
+      </div>
+    );
+  }
 }
 
 export default App;
