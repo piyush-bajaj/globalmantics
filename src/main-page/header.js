5bdc447464304fcc24a3982a0772b9de99c93196
Piyush Bajaj <45912468+piyush-bajaj@users.noreply.github.com>
2020-04-25 19:45:48 +0530

    header component

---
+++ src/main-page/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../main-page/GloboLogo.png";
+
+const Header = (props) => (
+  <header className="row">
+    <div className="col-md-5">
+      <img src={logo} className="logo" alt="logo"></img>
+    </div>
+    <div className="col-md-7 mt-5 subtitle">{props.subtitle}</div>
+  </header>
+);
+
+export default Header;
