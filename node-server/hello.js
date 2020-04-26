f61fb6ec46a3c68e877ed056e49e86afb48e1a78
Piyush Bajaj <45912468+piyush-bajaj@users.noreply.github.com>
2020-04-26 16:59:28 +0530

    serving json file from server

--- node-server/hello.js
+++ node-server/hello.js
@@ -1,12 +1,20 @@
 const http = require("http");
+const fs = require("fs").promises;
 
 const host = "localhost";
 const port = 8000;
 
 const requestListener = function (req, res) {
-  res.setHeader("Content-Type", "application/json");
-  res.writeHead(200);
-  res.end(`{"message" : "JSON message"}`);
+  fs.readFile("./houses.json")
+    .then((contents) => {
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(200);
+      res.end(contents);
+    })
+    .catch((err) => {
+      res.writeHead(500);
+      res.end(err);
+    });
 };
 
 const server = http.createServer(requestListener);
